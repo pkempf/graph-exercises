a89d96a5d7e9e135f431361898837175a42292b4
Peter Kempf <peterckempf@gmail.com>
2021-07-16 14:40:41 -0500

    navigated some graphs

---
+++ graph.js
@@ -0,0 +1,84 @@
+class Node {
+  constructor(value, adjacent = new Set()) {
+    this.value = value;
+    this.adjacent = adjacent;
+  }
+}
+
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+  }
+
+  // this function accepts a Node instance and adds it to the nodes property on the graph
+  addVertex(vertex) {
+      this.nodes.add(vertex);
+  }
+
+  // this function accepts an array of Node instances and adds them to the nodes property on the graph
+  addVertices(vertexArray) {
+      for (let vertex of vertexArray) this.addVertex(vertex);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to include the other vertex
+  addEdge(v1, v2) {
+      v1.adjacent.add(v2);
+      v2.adjacent.add(v1);
+  }
+
+  // this function accepts two vertices and updates their adjacent values to remove the other vertex
+  removeEdge(v1, v2) {
+      v1.adjacent.delete(v2);
+      v2.adjacent.delete(v1);
+  }
+
+  // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
+  removeVertex(vertex) {
+      for (let node of this.nodes) {
+          if (node.adjacent.has(vertex)) node.adjacent.delete(vertex);
+      }
+      this.nodes.delete(vertex);
+  }
+
+  // this function returns an array of Node values using DFS
+  depthFirstSearch(start) {
+      const seen = new Set();
+      const res = [];
+
+      function traverse(node) {
+          if (!node) return null;
+          seen.add(node);
+          res.push(node.value);
+          node.adjacent.forEach(neighbor => {
+              if (!seen.has(neighbor)) return traverse(neighbor);
+          })
+      }
+
+      traverse(start);
+      return res;
+  }
+
+  // this function returns an array of Node values using BFS
+  breadthFirstSearch(start) {
+      const queue = [start];
+      const res = [];
+      const seen = new Set();
+      let currentNode;
+
+      seen.add(start);
+      while (queue.length) {
+          currentNode = queue.shift();
+          res.push(currentNode.value);
+
+          currentNode.adjacent.forEach(neighbor => {
+              if (!seen.has(neighbor)) {
+                  seen.add(neighbor);
+                  queue.push(neighbor);
+              }
+          });
+      }
+      return res;
+  }
+}
+
+module.exports = {Graph, Node}
